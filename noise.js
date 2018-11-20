398566038a50ad33f1018ea8e9babbd3e9c50ed5
Ada <larmartinez2@gmail.com>
2018-11-19 16:57:41 -0800

    key press connected to sound

--- noise.js
+++ noise.js
@@ -1,7 +1,13 @@
 $(document).ready( function() {
   $('button').click(function() {
-    let letter = `${$(this).text()}`
+     let letter = `${$(this).text()}`
      let audio = document.getElementById(letter + "Audio");
      audio.play();
+  });
+
+  document.addEventListener('keypress', (event) => {
+    const keyName = event.key;
+    let audio = document.getElementById(keyName + "Audio");
+    audio.play();
   });
 });
