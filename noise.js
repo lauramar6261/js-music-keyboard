4d853e83852464f06c21245266aeddd87570e110
Ada <larmartinez2@gmail.com>
2018-11-19 16:31:25 -0800

    plays all sounds for all buttons

--- noise.js
+++ noise.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  $('.c').click(function() {
+  $('button').click(function() {
     let letter = `${$(this).text()}`
      let audio = document.getElementById(letter + "Audio");
      audio.play();
